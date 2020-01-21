c6702aa87eda611c4efbeee529b7ba8159116e42
EstudioVaiNaweb <cassioprazeres241@gmail.com>
2020-01-21 17:50:23 -0300

    fazendo seção adote estilização

---
+++ main.js
@@ -0,0 +1,42 @@
+let animals = [
+    {
+        img: "./img/Cao1.jpg",
+        nome: "Luna",
+        idade: "5 meses"
+    },
+    {
+        img: "./img/Cao2.jpg",
+        nome: "Duque",
+        idade: "2 anos"
+    },
+    {
+        img: "./img/Cao3.jpg",
+        nome: "Pérola",
+        idade: "1 ano e 4 meses"
+    },
+    {
+        img: "./img/Cao4.jpg",
+        nome: "Thor",
+        idade: "3 meses"
+    },
+    {
+        img: "./img/Cao5.jpg",
+        nome: "Vilma",
+        idade: "3 anos"
+    },
+    {
+        img: "./img/Cao6.jpg",
+        nome: "Princesa",
+        idade: "2 meses"
+    },
+    {
+        img: "./img/Cao10.jpg",
+        nome: "Eva",
+        idade: "3 anos"
+    },
+    {
+        img: "./img/Cao11.jpg",
+        nome: "Rex",
+        idade: "1 ano"
+    },
+]
