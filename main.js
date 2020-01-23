4b85f4f28f466e39f3008030df9a1217bb37a9b0
EstudioVaiNaweb <cassioprazeres241@gmail.com>
2020-01-23 16:59:57 -0300

    finalisando funcionalidade do slide

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Slide date
 let animals = [
     {
         img: "./img/Cao1.jpg",
@@ -40,3 +41,129 @@ let animals = [
         idade: "1 ano"
     },
 ]
+let contador = 4;
+let imgNumber = 0;  
+let saidaCao = [
+    {
+        img: document.getElementById('firstDog'),
+        nome: document.getElementById('firstName'),
+        idade: document.getElementById('firstAge')
+    },
+    {
+        img: document.getElementById('secondDog'),
+        nome: document.getElementById('secondName'),
+        idade: document.getElementById('secondAge')
+    },
+    {
+        img: document.getElementById('thirdDog'),
+        nome: document.getElementById('thirdName'),
+        idade: document.getElementById('thirdAge')
+    },
+    {
+        img: document.getElementById('fourthDog'),
+        nome: document.getElementById('fourthName'),
+        idade: document.getElementById('fourthAge')
+    }
+];
+let boxFrame = document.querySelector('.boxSlide-boxFrame')
+
+// Slide function next
+
+function nextSlide() {
+    
+    if (contador <= 8){
+        let setaNext = document.getElementById('arowNext');
+        let setaBack = document.getElementById('arowBack');
+        
+        console.log(contador, imgNumber);
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img);
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].nome;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        contador++ 
+        imgNumber++ 
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img);
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].imnomeg;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        contador++ 
+        imgNumber++ 
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img);
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].nome;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        contador++ 
+        imgNumber++ 
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img);
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].nome;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        console.log('entrou')
+        console.log(contador, imgNumber)
+        if (setaNext.style.display == "") {
+            setaNext.style.display = "none"
+            setaBack.style.display = "block"
+            boxFrame.style.margin = "1rem 0 0 5.6rem"
+        }
+        else {
+            setaNext.style.display = ""
+            setaBack.style.display = "none"
+            boxFrame.style.margin = "1rem 0 0 4.2rem"
+        }
+	}
+	else {
+        contador = 4
+        imgNumber = 0
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img);
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].nome;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        console.log(contador, imgNumber)
+		console.log('não entrou');
+	}
+
+}
+
+// Slide function back
+function backSlide() {
+    if (contador > 4){
+        let setaNext = document.getElementById('arowNext')
+        let setaBack = document.getElementById('arowBack')
+        
+        // console.log(contador, imgNumber)
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img);
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].nome;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        contador-- 
+        imgNumber-- 
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img);
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].imnomeg;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        contador-- 
+        imgNumber-- 
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img);
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].nome;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        contador-- 
+        imgNumber-- 
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img)
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].nome;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        console.log('entrou')
+        console.log(contador, imgNumber)
+        if (setaBack.style.display == "block"){
+            setaNext.style.display = ""
+            setaBack.style.display = "none"
+            boxFrame.style.margin = "1rem 0 0 4.2rem"
+        }
+        else {
+            setaNext.style.display = ""
+            setaBack.style.display = "none"
+            boxFrame.style.margin = "1rem 0 0 5.6rem"
+        }
+	}
+	else {
+        contador = 7
+        imgNumber = 3
+        saidaCao[imgNumber].img.setAttribute('src', animals[contador].img);
+        saidaCao[imgNumber].nome.innerHTML = animals[contador].nome;
+        saidaCao[imgNumber].idade.innerHTML = animals[contador].idade;
+        console.log(contador, imgNumber)
+		console.log('não entrou');
+	}
+}
